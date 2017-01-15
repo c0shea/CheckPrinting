a3394ab8a33c194285a78d61bf2b3b65d0842f8e
Connor O'Shea <connor@connoroshea.com>
2017-01-15 00:12:28 -0500

    Added icon

--- src/CheckPrinting/Main.Designer.cs
+++ src/CheckPrinting/Main.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.tabControl = new System.Windows.Forms.TabControl();
             this.tabPayee = new System.Windows.Forms.TabPage();
             this.datePicker = new System.Windows.Forms.DateTimePicker();
@@ -276,6 +277,7 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.tabControl);
             this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Main";
             this.Text = "Check Printing";
